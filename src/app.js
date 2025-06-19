325286e28d94101e39a23aba02e081819d8a4e5d
Nico Miras <73142724+nicolasmiras@users.noreply.github.com>
2025-06-19 01:04:32 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+
+  function domainFunction() {
+    let result = [];
+    for (let i=0; i<2; i++) {
+      for (let j=0; j<2; j++) {
+        for (let k=0; k<2; k++) {
+          result.push(pronoun[j] + adj[i] + noun[k] + '.com');
+        }
+      }
+    }
+    return result;
+  }
+
+  document.getElementById("domain").innerHTML = domainFunction().join('<br>');
 };
+
